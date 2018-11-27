ad1886d0c3d7ac2363aa4e623134b4b179f38860
RabeyaBibi <rabeya.cse950@gmail.com>
2018-11-27 16:45:04 -0600

    Nothing Changed; Just Updating from GitHub
    Signed-off-by: RabeyaBibi <rabeya.cse950@gmail.com>

--- src/Users/CurrentStaticUser.java
+++ src/Users/CurrentStaticUser.java
@@ -5,4 +5,16 @@ public class CurrentStaticUser {
     public static String username;
     public static String password;
     public static int teamId;
+    public static String role;
+    public static String email;
+
+    /*
+  userID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  password varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  isApproved tinyint(1) DEFAULT '0',
+  email varchar(50) UNIQUE NOT NULL,
+  teamID INT UNSIGNED DEFAULT NULL,
+    * */
 }
--- src/Users/Reviewer.java
+++ src/Users/Reviewer.java
@@ -2,25 +2,43 @@ package Users;
 
 import Others.Artifact;
 import Reviews.ArtifactReview;
+import main.Database;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Reviewer extends User {
 
-    // ASSOCIATION: Reviewer - Artifact: ?? List of single ??
-    // Can Reviewer be assigned to several artifacts at the same time??
+    // Can Reviewer be assigned to several artifacts at the same time?? -- yes, they can
     ArrayList<Artifact> reviews = new ArrayList<>();
-
-    // ASSOCIATION: Reviewer - ArtifactReview: ??? NO NEED ???
 
 
     public ArtifactReview reviewArtifact(int artifactID, int rating, String description)
     {
         ArtifactReview dummy = new ArtifactReview();
-
-
-
         return dummy;
+    }
+
+    public ArrayList<User> showListOfReviewers() throws SQLException {
+        Connection conn = Database.createConnection();
+        ArrayList<CurrentStaticUser> reviewers = new ArrayList<>();
+
+        String query = "SELECT * FROM USER WHERE role = Reviewer" ;
+        Statement statement = conn.createStatement();
+        ResultSet resultset = statement.executeQuery(query);
+
+        CurrentStaticUser temp = new CurrentStaticUser();
+        while(resultset.next()){
+            temp.userId = resultset.getInt("userID");
+            temp.username = resultset.getString("username");
+            temp.password = resultset.getString("password");
+            temp.teamId = resultset.getInt("teamId");
+            temp.role  = resultset.getString("role");
+            temp.email = resultset.getString("email");
+            reviewers.add(temp);
+        }
+
+        return reviewers;
     }
 
     public Reviewer(String username, int userID) {

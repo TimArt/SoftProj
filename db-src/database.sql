e6cdcbe3ca10bb7eef43ce4d0e8d076a241ec4dc
Tim Arterbury <timarterbury@gmail.com>
2018-11-27 09:10:47 -0600

    Updates schema to have submission

--- db-src/database.sql
+++ db-src/database.sql
@@ -1,45 +1,23 @@
-CREATE DATABASE  IF NOT EXISTS `softproj` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+CREATE DATABASE IF NOT EXISTS `softproj`
+    DEFAULT CHARACTER SET UTF8MB4
+    DEFAULT COLLATE utf8mb4_general_ci;
+    
 USE `softproj`;
--- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
---
--- Host: localhost    Database: softproj
--- ------------------------------------------------------
--- Server version	8.0.13
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
---
--- Table structure for table Team
---
+SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS Team;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-SET character_set_client = utf8mb4 ;
+
 CREATE TABLE Team (
     
     teamID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    isApproved BOOLEAN NOT NULL DEFAULT FALSE
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    isApproved BOOLEAN NOT NULL DEFAULT FALSE  
+)
+ENGINE=InnoDB;
 
---
--- Table structure for table User
---
 
 DROP TABLE IF EXISTS `User`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-SET character_set_client = utf8mb4 ;
+
 CREATE TABLE `User` (
   userID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   name varchar(45) NOT NULL,
@@ -50,42 +28,35 @@ CREATE TABLE `User` (
   teamID INT UNSIGNED DEFAULT NULL,
   
   FOREIGN KEY (teamID) REFERENCES Team (teamID)
+)
+ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS Submission;
+
+CREATE TABLE Submission (
+    
+    submissionID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    teamID INT UNSIGNED NOT NULL,
+    submitterID INT UNSIGNED NOT NULL,
+    dateSubmitted DATETIME NOT NULL,
   
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    FOREIGN KEY (teamID) REFERENCES Team (teamID),
+    FOREIGN KEY (submitterID) REFERENCES `User` (userID)
+)
+ENGINE=InnoDB;
 
-
---
--- Table structure for table Artifact
---
 
 DROP TABLE IF EXISTS Artifact;
 
-SET character_set_client = utf8mb4 ;
 CREATE TABLE Artifact (
     name varchar(50) NOT NULL,
     phase varchar(45) NOT NULL,
     type varchar(45) NOT NULL,
     directory varchar(150) NOT NULL,
-    teamID INT UNSIGNED NOT NULL,
-    submitterID INT UNSIGNED NOT NULL,
-  
-    FOREIGN KEY (teamID) REFERENCES Team (teamID),
-    FOREIGN KEY (submitterID) REFERENCES `User` (userID)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    submissionID INT UNSIGNED NOT NULL,
+    
+    FOREIGN KEY (submissionID) REFERENCES Submission (submissionID)
+)
+ENGINE=InnoDB;
 
 
---
--- Dumping routines for database 'softproj'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-11-26 20:03:11
